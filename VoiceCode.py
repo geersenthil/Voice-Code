9671f2362fde362ca37d2e9f368158eb3d6ed9f6
geersenthil <geerthika.senthil@gmail.com>
2021-07-15 09:10:23 -0400

    testing prediction accuracy

--- VoiceCode.py
+++ VoiceCode.py
@@ -104,3 +104,227 @@ def get_spectrogram(waveform):
 
   return spectrogram
 
+# spectogram + label
+def get_spectrogram_and_label_id(audio, label):
+  spectrogram = get_spectrogram(audio)
+  spectrogram = tf.expand_dims(spectrogram, -1)
+  label_id = tf.argmax(label == commands)
+  return spectrogram, label_id
+
+  # convert train files to spectograms
+spectrogram_ds = waveform_ds.map(
+    get_spectrogram_and_label_id, num_parallel_calls=AUTOTUNE)
+
+
+   # wrapper combining the above few files
+def preprocess_dataset(files):
+  # get files
+  files_ds = tf.data.Dataset.from_tensor_slices(files)
+  # waveform/labels of files
+  output_ds = files_ds.map(get_waveform_and_label, num_parallel_calls=AUTOTUNE)
+  # spectogram/labels of files
+  output_ds = output_ds.map(
+      get_spectrogram_and_label_id,  num_parallel_calls=AUTOTUNE)
+  return output_ds
+
+
+  # below 2 are identical
+train_ds = spectrogram_ds
+train_ds = preprocess_dataset(train_files)
+val_ds = preprocess_dataset(val_files)
+test_ds = preprocess_dataset(test_files)
+
+
+# set batc sizes for training
+batch_size = 64
+train_ds = train_ds.batch(batch_size)
+val_ds = val_ds.batch(batch_size)
+
+
+# get the input shape
+for spectrogram, _ in spectrogram_ds.take(1):
+  input_shape = spectrogram.shape
+print('Input shape:', input_shape)
+num_labels = len(commands)
+
+# create normalization layer
+norm_layer = preprocessing.Normalization()
+norm_layer.adapt(spectrogram_ds.map(lambda x, _: x))
+
+# add layers to model
+model = models.Sequential([
+    layers.Input(shape=input_shape),
+    preprocessing.Resizing(32, 32), 
+    norm_layer,
+    layers.Conv2D(32, 3, activation='relu'),
+    layers.Conv2D(64, 3, activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Dropout(0.25),
+    layers.Flatten(),
+    layers.Dense(1024, activation='relu'),
+    layers.Dropout(0.5),
+    layers.Flatten(),
+    layers.Dense(512, activation='relu'),
+    layers.Dropout(0.5),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dropout(0.5),
+    layers.Dense(num_labels),
+])
+
+model.summary()
+
+
+# compile
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy'],
+)
+
+# train
+EPOCHS = 15
+history = model.fit(
+    train_ds, 
+    validation_data=val_ds,  
+    epochs=EPOCHS,
+    callbacks=tf.keras.callbacks.EarlyStopping(verbose=1, patience=2),
+)
+
+
+# graphical overview
+metrics = history.history
+plt.plot(history.epoch, metrics['loss'], metrics['val_loss'])
+plt.legend(['loss', 'val_loss'])
+plt.show()
+
+# prepare test
+test_audio = []
+test_labels = []
+
+for audio, label in test_ds:
+  test_audio.append(audio.numpy())
+  test_labels.append(label.numpy())
+
+test_audio = np.array(test_audio)
+test_labels = np.array(test_labels)
+
+# predict on test data
+y_pred = np.argmax(model.predict(test_audio), axis=1)
+y_true = test_labels
+
+test_acc = sum(y_pred == y_true) / len(y_true)
+print(f'Test set accuracy: {test_acc:.0%}')
+
+
+# try on one file
+sample_file = data_dir/'8/8_george_1.wav'
+
+sample_ds = preprocess_dataset([str(sample_file)])
+
+for spectrogram, label in sample_ds.batch(1):
+  prediction = model(spectrogram)
+  plt.bar(commands, tf.nn.softmax(prediction[0]))
+  plt.title(f'Predictions for "{commands[label[0]]}"')
+  plt.show()
+
+# save model
+saved_model_path = os.path.join(pathlib.Path('saved_model'), "saved_model")
+tf.saved_model.save(model, saved_model_path)
+
+# convert saved model to tflite
+converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_path) # path to the SavedModel directory
+tflite_model = converter.convert()
+
+# Save the model.
+with open('model.tflite', 'wb') as f:
+  f.write(tflite_model)
+
+
+# load saved model
+saved_model = tf.saved_model.load(saved_model_path)
+
+# try example on saved model
+for spectrogram, label in sample_ds.batch(1):
+  prediction = saved_model(spectrogram)
+  plt.bar(commands, tf.nn.softmax(prediction[0]))
+  plt.title(f'Predictions for "{commands[label[0]]}"')
+  plt.show()
+
+
+# try on another example
+file_string = os.path.join(pathlib.Path('test_file'),'2_george_0.wav')
+test_file = pathlib.Path(file_string)
+
+ptf = preprocess_dataset([str(test_file)])
+
+for spectrogram, label in ptf.batch(1):
+  prediction = saved_model(spectrogram)
+  print("Predicted: {}".format(np.argmax(prediction)))
+
+
+# for python script
+import os
+import pathlib
+import numpy as np
+import tensorflow as tf
+
+# Autotune for loading
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+# tf function to decode wav file
+def decode_audio(audio_binary):
+  audio, _ = tf.audio.decode_wav(audio_binary)
+  return tf.squeeze(audio, axis=-1)
+
+
+# waveform from decoded audio
+def get_waveform(file_path):
+#   label = get_label(file_path)
+  audio_binary = tf.io.read_file(file_path)
+  waveform = decode_audio(audio_binary)
+  return waveform
+
+
+# spectogram of the audio clip from waveform
+def get_spectrogram(waveform):
+  # Padding for files with less than 16000 samples
+  zero_padding = tf.zeros([19000] - tf.shape(waveform), dtype=tf.float32)
+
+  # Concatenate audio with padding so that all audio clips will be of the 
+  # same length
+  waveform = tf.cast(waveform, tf.float32)
+  equal_length = tf.concat([waveform, zero_padding], 0)
+  spectrogram = tf.signal.stft(
+      equal_length, frame_length=255, frame_step=128)
+
+  spectrogram = tf.abs(spectrogram)
+    
+  spectrogram = tf.expand_dims(spectrogram, -1)
+
+  return spectrogram
+
+
+def preprocess_data(files):
+  # get files
+  files_ds = tf.data.Dataset.from_tensor_slices(files)
+  output_ds = files_ds.map(get_waveform, num_parallel_calls=AUTOTUNE)
+  output_ds = output_ds.map(
+      get_spectrogram,  num_parallel_calls=AUTOTUNE)
+  return output_ds
+
+  saved_model_path = os.path.join(pathlib.Path('saved_model'), "saved_model")
+# load saved model
+saved_model = tf.saved_model.load(saved_model_path)
+
+
+# in the example, test_file is a dir which contains 2_george_0.wav
+file_string = os.path.join(pathlib.Path('test_file'),'2_george_0.wav')
+test_file = pathlib.Path(file_string)
+
+ptf = preprocess_data([str(test_file)])
+
+# take the example from ptf, and run saved_model with it
+for spectrogram in ptf.batch(1):
+  prediction = saved_model(spectrogram)
+  print("Predicted: {}".format(np.argmax(prediction)))
